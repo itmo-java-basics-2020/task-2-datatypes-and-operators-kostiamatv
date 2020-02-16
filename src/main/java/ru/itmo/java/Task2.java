b8616a899f173978e8848399aa99ae6f5c096290
(◕‿◕) <kostiamatv@gmail.com>
2020-02-16 20:59:02 +0300

    tasks done

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -11,21 +11,21 @@ public class Task2 {
      * - вывод 0.9
      */
     double getFractionalPart(double realNumber) {
-        return 0.0; //your code here
+        return realNumber % 1; //your code here
     }
 
     /**
      * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
      */
     int charCode(char c) {
-        return 0; //your code here
+        return (int) c; //your code here
     }
 
     /**
      * Даны 2 целых числа. Проверьте равны ли они
      */
     boolean testIfIntsEqual(Integer a, Integer b) {
-        return false; //your code here
+        return a.equals(b); //your code here
     }
 
     /**
@@ -38,35 +38,49 @@ public class Task2 {
      * @param inclusively входят ли границы в заданный даипазон
      */
     boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively) {
-        return false; //your code here
+        if (number == null || leftBound == null || rightBound == null || inclusively == null) {
+            return  false;
+        }
+        if(rightBound - leftBound < 0){
+            return false;
+        }
+        if (inclusively){
+            return number.equals(leftBound) || number.equals(rightBound)
+                    || ((number - leftBound) > 0 && (rightBound - number) > 0) ;
+        }
+        else{
+            return ((number - leftBound) > 0 && (rightBound - number) > 0);
+        }
     }
 
     /**
      * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
      */
     boolean atLeastOneIsDigit(char c1, char c2, char c3) {
-        return false; //your code here
+        return numberInRange((int) c1, (int) '0', (int) '9', true) ||
+                numberInRange((int) c2, (int) '0', (int) '9', true) ||
+                numberInRange((int) c3, (int) '0', (int) '9', true); //your code here
     }
 
     /**
      * Даны два вещественных числа. Определите равны ли они
      */
     boolean areRealNumbersEqual(double a, double b) {
-        return false; //your code here
+        return (-1e-10 < a - b && a - b < 1e-10); //your code here
     }
 
     /**
      * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
      */
     int sumOfDigits(int n) {
-        return 0; //your code here
+        return n / 100 + n % 100 / 10 + n % 10; //your code here
     }
 
     /**
      * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
      */
     int nextEvenNumber(int n) {
-        return 0; //your code here
+        return (n / 2 + 1) * 2; //your code here
     }
 
     /**
@@ -76,14 +90,14 @@ public class Task2 {
      * своем кабинете
      */
     int schoolDesks(int num1, int num2, int num3) {
-        return 0; //your code here
+        return (num1 + 1) / 2 + (num2 + 1) / 2 + (num3 + 1) / 2; //your code here
     }
 
     /**
      * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
      */
     int xorDigits(int N) {
-        return 0; //your code here
+        return (N % 100 / 10 ^ N % 10); //your code here
     }
 
 }
